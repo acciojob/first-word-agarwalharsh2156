cb0ba54c93179d722aaebcd8490bf8f9916ee952
Harsh Agarwal <agarwalharsh2156@gmail.com>
2025-06-17 22:15:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
-  // your code here
+  s = s.trim(); // Remove leading/trailing whitespace
+  let index = s.indexOf(' ');
+  if (index === -1) return s;  // No space found
+  return s.slice(0, index);    // Return substring up to first space
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
